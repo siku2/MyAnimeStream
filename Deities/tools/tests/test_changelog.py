c2b155f0ca64e9b3ae8fc38f2a6face2d652203f
Simon Berger <siku2@outlook.de>
2018-06-29 16:37:45 +0200

    adding a small parser test

--- Deities/tools/tests/test_changelog.py
+++ Deities/tools/tests/test_changelog.py
@@ -1,2 +1,14 @@
-def test_test():
-    print("test")
+from datetime import datetime
+from pathlib import Path
+
+from ..changelog import upload
+
+test_file = Path(__file__).parent / "changelog.log"
+
+
+def test_parser():
+    changelog = upload.parse_changelog(test_file.read_text())
+    assert changelog["version_num"] == 4294967296
+    assert changelog["release"] == datetime(2018, 6, 28)
+    changes = changelog["changes"]
+    assert len(changes) == 3
