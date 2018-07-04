1ee59050b863ce9a64a56b85a635569c9e65574c
Simon Berger <siku2@outlook.com>
2018-07-04 15:16:15 +0200

    markdown formatting for changelogs

--- Deities/tools/changelog/upload.py
+++ Deities/tools/changelog/upload.py
@@ -67,18 +67,67 @@ def parse_header(text: str) -> ChangelogHeader:
     return ChangelogHeader(version, release)
 
 
+def get_version_code(*parts) -> int:
+    return sum(part << (len(parts) - i) * 16 for i, part in enumerate(parts, 1))
+
+
+def get_version(code: int):
+    return tuple((code & (16 ** (4 * i) - 1)) >> ((i - 1) * 16) for i in range(3, 0, -1))
+
+
 def parse_changelog(text: str) -> dict:
     header, changes = text.split(FILE_SPLITTER, 1)
     header = parse_header(header)
     changes = parse_logs(changes)
 
-    version_num = sum(part << (len(header.version) - i) * 16 for i, part in enumerate(header.version, 1))
+    version_num = get_version_code(*header.version)
 
     return {
         "version_num": version_num,
         "release": header.release,
         "changes": changes
     }
+
+
+def format_json(changelog: dict) -> str:
+    import json
+    from json import JSONEncoder
+
+    class ConsoleEncoder(JSONEncoder):
+        def default(self, o: Any) -> Any:
+            if isinstance(o, datetime):
+                return o.isoformat()
+            return super().default(o)
+
+    return json.dumps(changelog, cls=ConsoleEncoder, indent=4)
+
+
+def format_markdown(changelog: dict) -> str:
+    from collections import defaultdict
+    from operator import itemgetter
+
+    version = ".".join(map(str, get_version(changelog["version_num"])))
+    release = changelog["release"].strftime("%d %b %Y")
+
+    markdown = [f"# Version {version} ({release})"]
+
+    categories = defaultdict(list)
+    for change in changelog["changes"]:
+        log_type = change["type"]
+        text = change["text"]
+        priority = change.get("priority", 0)
+        categories[log_type].insert(0, (text, priority))
+
+    for category_changes in categories.values():
+        category_changes.sort(key=itemgetter(1), reverse=True)
+
+    for category, changes in categories.items():
+        markdown.append(f"### {category}")
+        for change, priority in changes:
+            change = f"**{change}**" if priority else change
+            markdown.append(f"- {change}")
+
+    return "\n".join(markdown)
 
 
 if __name__ == "__main__":
@@ -92,18 +141,11 @@ if __name__ == "__main__":
     changelog = parse_changelog(args.file.read())
 
     if args.output == "json":
-        import json
-        from json import JSONEncoder
-
-
-        class ConsoleEncoder(JSONEncoder):
-            def default(self, o: Any) -> Any:
-                if isinstance(o, datetime):
-                    return o.isoformat()
-                return super().default(o)
-
-
-        print(json.dumps(changelog, cls=ConsoleEncoder, indent=4))
+        print(format_json(changelog))
+    elif args.output == "markdown":
+        print("=====")
+        print(format_markdown(changelog))
+        print("=====")
     elif args.output.startswith("mongodb://"):
         from pymongo import MongoClient
 
