1718627cce0fc1cb80699a42ce96952751ca08f8
Simon Berger <siku2@outlook.com>
2018-07-09 23:51:24 +0200

    updating outdated tests

--- Grobber/tests/test_streams/__init__.py
+++ Grobber/tests/test_streams/__init__.py
@@ -9,14 +9,13 @@ class BasicStreamTest:
         self.requests = list(map(Request, self.TESTS))
         self.streams = list(map(self.CLS, self.requests))
 
-    def test_can_handle(self):
+    def test_requests(self):
         for req in self.requests:
+            print("looking at", req)
             assert self.CLS.can_handle(req)
 
-    def test_poster(self):
+    def test_streams(self):
         for stream in self.streams:
+            print("looking at", stream)
             assert stream.poster
-
-    def test_links(self):
-        for stream in self.streams:
             assert stream.links
--- Grobber/tests/test_streams/test_mp4upload.py
+++ Grobber/tests/test_streams/test_mp4upload.py
@@ -8,5 +8,7 @@ from . import BasicStreamTest
 class TestMp4Upload(BasicStreamTest, TestCase):
     CLS = Mp4Upload
     TESTS = [
-        "https://www.mp4upload.com/embed-c7fehm6nsprf.html"
+        "https://www.mp4upload.com/embed-h2yq5i3c7xo7.html",
+        "https://www.mp4upload.com/embed-1934b3ai70n2.html",
+        "https://www.mp4upload.com/embed-8yajb93uspci.html"
     ]
