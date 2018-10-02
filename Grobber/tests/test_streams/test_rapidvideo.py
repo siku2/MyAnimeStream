198fc69eaf76dfd9d0c4e3d0227c46e66dab117a
Simon Berger <siku2@outlook.com>
2018-10-02 20:56:04 +0200

    rapidvideo still ded

--- Grobber/tests/test_streams/test_rapidvideo.py
+++ Grobber/tests/test_streams/test_rapidvideo.py
@@ -1,10 +1,11 @@
 from unittest import TestCase
 
+import pytest
+
 from grobber.streams.rapidvideo import RapidVideo
 from . import BasicStreamTest
 
-
-# pytestmark = pytest.mark.skip("RapidVideo is having problems")
+pytestmark = pytest.mark.skip("RapidVideo extraction is having problems")
 
 
 class TestRapidVideo(BasicStreamTest, TestCase):
